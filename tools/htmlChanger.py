1ec0b74a432f966d3ff7e356c5d312ea963d9703
matzTada <matz@west.sd.keio.ac.jp>
2017-06-21 16:53:21 +0900

    tried replace

--- tools/htmlChanger.py
+++ tools/htmlChanger.py
@@ -8,3 +8,7 @@ soup = BeautifulSoup(html, "html.parser")
 #print(soup.body.header.div.a.find_all("span"))
 
 print(soup.find("article", attrs={"class" : "style3"}).prettify())
+
+soup.find("article", attrs={"class" : "style3"}).find("h2").replace_with("hogehogehoge")
+
+print(soup.find("article", attrs={"class" : "style3"}).prettify())
