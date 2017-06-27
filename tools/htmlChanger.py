4fb5d239aa73ec39c26648e23c00df7c99c334a2
matzTada <matz@west.sd.keio.ac.jp>
2017-06-27 15:23:03 +0900

    succeed in changing type of artcile

--- tools/htmlChanger.py
+++ tools/htmlChanger.py
@@ -1,28 +1,32 @@
+#htmlChanger.py
+#variables
+inFileName = "../index.html"
+outFileName = "../index_mod.html"
+
 #read from file
 import codecs
-f = codecs.open("test.html", "r")
+f = codecs.open(inFileName, "r")
 html = f.read()
 
+#using BeautifulSoup to handle html files
 from bs4 import BeautifulSoup
 soup = BeautifulSoup(html, "html.parser")
 
-#print(soup.body.header.div.a.find_all("span"))
-
-#for tmpArticle in soup.find_all("article", attrs={"class" : "style4"}):
-
 for tmpArticle in soup.find_all("article"): 
-    print(tmpArticle.find("h2").string + " " + tmpArticle["class"][0].encode())
+    print(tmpArticle.find("h2").string + " : " + tmpArticle["class"][0].encode())
+    tmpArticle["class"][0] = "style0"
     
-print("=== before ===")
-print(soup.find("article", attrs={"class" : "style4"}).prettify())
-
-print("=== change ===")
-soup.find("article", attrs={"class" : "style4"}).find("h2").string.replace_with("NEW!!")
-
-print("=== after ===")
-print(soup.find("article").prettify())
-
 #write to files
-outputFile = open("new.html", "w")
+outputFile = open(outFileName, "w")
 outputFile.write(soup.prettify(formatter="html"))
 outputFile.close()
+
+#unused codes
+#print("=== before ===")
+#print(soup.find("article", attrs={"class" : "style4"}).prettify())
+#print("=== change ===")
+#soup.find("article", attrs={"class" : "style4"}).find("h2").string.replace_with("NEW!!")
+#print("=== after ===")
+#print(soup.find("article").prettify())
+#print(soup.body.header.div.a.find_all("span"))
+#for tmpArticle in soup.find_all("article", attrs={"class" : "style4"}):
