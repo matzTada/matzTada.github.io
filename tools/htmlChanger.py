3695bb94c9c2fb597696df959c2e121a6c087551
TadaMatz <matz@west.sd.keio.ac.jp>
2017-06-27 13:53:50 +0900

    enable multiple artcile handling

--- tools/htmlChanger.py
+++ tools/htmlChanger.py
@@ -1,3 +1,4 @@
+#read from file
 import codecs
 f = codecs.open("test.html", "r")
 html = f.read()
@@ -7,15 +8,21 @@ soup = BeautifulSoup(html, "html.parser")
 
 #print(soup.body.header.div.a.find_all("span"))
 
+#for tmpArticle in soup.find_all("article", attrs={"class" : "style4"}):
+
+for tmpArticle in soup.find_all("article"): 
+    print(tmpArticle.find("h2").string + " " + tmpArticle["class"][0].encode())
+    
 print("=== before ===")
 print(soup.find("article", attrs={"class" : "style4"}).prettify())
 
 print("=== change ===")
-soup.find("article", attrs={"class" : "style4"}).find("h2").string.replace_with("hogehogehoge")
+soup.find("article", attrs={"class" : "style4"}).find("h2").string.replace_with("NEW!!")
 
 print("=== after ===")
 print(soup.find("article").prettify())
 
+#write to files
 outputFile = open("new.html", "w")
 outputFile.write(soup.prettify(formatter="html"))
 outputFile.close()
