b8280222e49afa4aaa4b25d4537f0f47d02634de
matzTada <matz@west.sd.keio.ac.jp>
2017-07-05 18:38:13 +0900

    tested colorChanger by JavaScript

---
+++ tools/colorChanger.js
@@ -0,0 +1,20 @@
+// var colorArray = ["style1", "style2", "style3", "style4", "style5", "style6", "style7"]
+var colorArray = ["style1", "style2"]
+var colorArrayItr = 0
+
+var arts = document.getElementsByTagName("article");
+for ( var i = 0; i < arts.length; i++){
+    var tmpClassName = arts[i].getAttribute("class");
+    console.log(tmpClassName);
+	 
+    if (tmpClassName != "style0"){
+	    arts[i].className = colorArray[colorArrayItr]    	
+	    colorArrayItr += 1;
+	    if(colorArrayItr >= colorArray.length){
+	        colorArrayItr = 0;
+	    }
+    }
+    else{
+	    arts[i].className = "style0";       	
+    }
+}
